e8f5fc81c6d530057339e73fd43d4a6a3440080f
Thinksysuser <Thinksysuser@10.101.23.91>
2021-03-04 11:17:48 +0530

    Commit

---
+++ selenium_maven/src/main/java/Im_Star/selenium_maven/App.java
@@ -0,0 +1,13 @@
+package Im_Star.selenium_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
