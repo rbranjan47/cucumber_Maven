249ec1a0a175e3b93754028482a1f8cd2232708b
Thinksysuser <Thinksysuser@10.101.23.91>
2021-03-09 12:10:10 +0530

    Commited

--- Selenium_Maven/src/test/java/Selenium/Selenium_Maven/privacy_links.java
+++ Selenium_Maven/src/test/java/Selenium/Selenium_Maven/privacy_links.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class broken_links 
+public class privacy_links 
 {
 	public static WebDriver driver;
 	public static JavascriptExecutor js;
