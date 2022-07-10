5d061951bfb92f8e8a87d168c2c1bc413ec99ed9
rbranjan47 <rbranjan47@gmail.com>
2022-07-11 00:42:35 +0530

    11th July

--- Selenium_Maven/src/test/java/Selenium/Selenium_Maven/privacy_links.java
+++ Selenium_Maven/src/test/java/Selenium/Selenium_Maven/privacy_links.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -15,32 +16,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class privacy_links 
-{
-	public static WebDriver driver;
-	public static JavascriptExecutor js;
-	public static long timeout = 20;
-	public static long pageload = 20;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws InterruptedException, MalformedURLException, IOException 
-	{
-		System.setProperty("webdriver.chrome.driver", "D:\\rabi_thinksys\\chromedriver_win32\\chromedriver.exe");
-		 driver = new ChromeDriver();
-		driver = new ChromeDriver();
+public class privacy_links {
+	public static JavascriptExecutor js;
+
+	public static void main(String[] args) throws InterruptedException, MalformedURLException, IOException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
 
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(pageload, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
 		driver.get("https://freecrm.com/");
 
 		@SuppressWarnings("unused")
-		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		WebElement login = driver.findElement(
 				By.xpath("//a[@class='btn btn-primary btn-xs-2 btn-shadow btn-rect btn-icon btn-icon-left']"));
 		driver.getTitle();
-		//wait.until(ExpectedConditions.elementToBeClickable(login));
+		// wait.until(ExpectedConditions.elementToBeClickable(login));
 		login.click();
 		driver.getTitle();
 		driver.findElement(By.xpath("//input[@placeholder='E-mail address']")).sendKeys("rbranjan47@gmail.com");
@@ -48,66 +44,57 @@ public class privacy_links
 
 		driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']")).click();
 		Thread.sleep(4000);
-		
-		//driver.switchTo().frame("mainpanel");
-		
-		//getting all links in the webpage
-		List<WebElement> links=driver.findElements(By.tagName("a"));
+
+		// driver.switchTo().frame("mainpanel");
+
+		// getting all links in the webpage
+		List<WebElement> links = driver.findElements(By.tagName("a"));
 		links.addAll(driver.findElements(By.tagName("img")));
-		//all links
-		System.out.println("All links present on the Page is "+links.size());
-		
-		List<WebElement> activelinks=new ArrayList<WebElement>();
-		//adding on the Active links
-		for (int i=0; i<links.size();i++)
-		{
-			if(links.get(i).getAttribute("href") != null && !links.get(i).getAttribute("href").contains("javascript"))
-			{
+		// all links
+		System.out.println("All links present on the Page is " + links.size());
+
+		List<WebElement> activelinks = new ArrayList<WebElement>();
+		// adding on the Active links
+		for (int i = 0; i < links.size(); i++) {
+			if (links.get(i).getAttribute("href") != null
+					&& !links.get(i).getAttribute("href").contains("javascript")) {
 				activelinks.addAll(links);
 			}
 		}
-		System.out.println("active links are "+ activelinks.size());
-		
-		//checking the Links is correct by HTTPS API
-		try
-		{
-		for (int j=0; j<activelinks.size();j++)
-		{
-			//we can verify by making connection with the help of HTTPConnection class
-			HttpURLConnection connection=(HttpURLConnection) new URL(activelinks.get(j).getAttribute("href")).openConnection();
-			//connecting
-			connection.connect();
-			//checking the response 
-			String response=connection.getResponseMessage();
-			long responsecode=connection.getResponseCode();
-			//disconnect
-			connection.disconnect();
-			
-			System.out.println("Response is "+activelinks.get(j).getAttribute("href")+response);
-			
-			if(responsecode>=400)
-			{
-				System.out.println("Broken links are "+ activelinks);
+		System.out.println("active links are " + activelinks.size());
+
+		// checking the Links is correct by HTTPS API
+		try {
+			for (int j = 0; j < activelinks.size(); j++) {
+				// we can verify by making connection with the help of HTTPConnection class
+				HttpURLConnection connection = (HttpURLConnection) new URL(activelinks.get(j).getAttribute("href"))
+						.openConnection();
+				// connecting
+				connection.connect();
+				// checking the response
+				String response = connection.getResponseMessage();
+				long responsecode = connection.getResponseCode();
+				// disconnect
+				connection.disconnect();
+
+				System.out.println("Response is " + activelinks.get(j).getAttribute("href") + response);
+
+				if (responsecode >= 400) {
+					System.out.println("Broken links are " + activelinks);
+				} else {
+					System.out.println("Valid links " + activelinks);
+				}
+
 			}
-			else
-			{
-				System.out.println("Valid links "+activelinks);
-			}
-			
-		}
-		}
-		catch(MalformedURLException e)
-		{
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		driver.findElement(By.xpath("//i[@class='settings icon']/../../div/i")).click();
-		WebElement logout=driver.findElement(By.xpath("//span[contains(text(),'Log Out')]"));
-		//wait.until(ExpectedConditions.elementToBeClickable(logout));
+		WebElement logout = driver.findElement(By.xpath("//span[contains(text(),'Log Out')]"));
+		// wait.until(ExpectedConditions.elementToBeClickable(logout));
 		logout.click();
 		driver.quit();
-		
 
 	}
 }
